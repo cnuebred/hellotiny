e3605a795e9a8e66ad663b0dfdbd5c0cfb28c20c
cnuebred <jonatann01ew@gmail.com>
2025-02-17 01:13:30 +0100

    update utils + develop main; TODO update readme

--- src/main.c
+++ src/main.c
@@ -7,11 +7,18 @@
 #include "twi/uart.h"
 #include "twi/i2c.h"
 
+#include "ssd1306/ssd1306.h"
+
 
 int main(void)
 {
+    i2c_init();
+
+    oled_init();
+    oled_fill_black();
     while (true)
     {
-        
+        oled_write_text(34, 3, "HELLO TINY");
+        _delay_ms(1000);
     }
 }
