a390df7610670d83cbaba5ac809077d39fc0a91e
cnuebred <jonatann01ew@gmail.com>
2025-02-17 01:12:32 +0100

    fix send_package function: https://stackoverflow.com/a/10349610

--- src/twi/i2c.c
+++ src/twi/i2c.c
@@ -1,12 +1,14 @@
-
 #include "i2c.h"
 
+/*
+  Why `_size_t size`? 
+   - https://stackoverflow.com/a/10349610
+*/
 
-void send_package(byte * data){
+void send_package(const byte * data, _size_t size){
   i2c_start();
-  while(*data){
-    i2c_write_byte(*data);
-    data++;
+  for (int i = 0; i < size; i++){
+    i2c_write_byte(data[i]);
   }
   i2c_stop();
 }
